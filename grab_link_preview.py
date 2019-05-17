ea9d502027acf28886f59dffa0d05627bb6fa446
Pedro Moraes <pedromoraes@gmail.com>
2019-05-16 23:15:44 -0300

    grabtry

--- grab_link_preview.py
+++ grab_link_preview.py
@@ -2,9 +2,12 @@ from webpreview import web_preview
 from string import Template
 
 def grab_link_preview(url):
-    title, description, image = web_preview(url)
-    if description:
-        t = '[pcclinkpreview url="$url" title="$title" image="$image"]$description[/pcclinkpreview]'
-        return Template(t).safe_substitute(locals())
+    try:
+        title, description, image = web_preview(url)
+        if description:
+            t = '[pcclinkpreview url="$url" title="$title" image="$image"]$description[/pcclinkpreview]'
+            return Template(t).safe_substitute(locals())
+    except:
+        print('error grabbing url')
 
     return Template('<a href="$url">$url</a>').substitute(dict(url=url))
